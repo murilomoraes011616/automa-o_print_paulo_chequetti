d1e3da22bc2eecf7a866b73e5a0d97f5367361cb
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-30 09:16:56 -0300

    cometi um erro na trocaa dos arquivos, voltando pro original

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ aba.api.ExportAsFixedFormat(0, fr'C:\Users\murilo.oliveira\OneDrive - Greentech\
 from playwright.sync_api import sync_playwright
 
 caminho_da_sessao = r'C:\Users\murilo.oliveira\OneDrive - Greentech\Perfil\Desktop\sessao_whatsapp'
-nome_do_contato = "Patricia Chefe"
-caminho_do_pdf= r'\\viaduto.corp\fs\AREA_DE_DADOS\Indicadores\Gestao de Contratos\FILIAL SP\KPI - Faturamento\Mapa de Faturamento\Mapa_Faturamento_SAPHANA_Automatizado_Murilo_Moraes.xlsm'
+nome_do_contato = "Murilo , da MITRA Assessoria IA"
+caminho_do_pdf= fr'C:\Users\murilo.oliveira\OneDrive - Greentech\Perfil\Desktop\pastas para coisas da  automações\arquivos da automação para o paulo chequeti\lugar dos pdf para autoamação\Mapa de Faturamento {hoje_formatado}.pdf'
 
 with sync_playwright() as p:
     navegador = p.chromium.launch_persistent_context(
