de8416ea83dc40837ad7cf84467ec3bb9e0389fe
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-08-11 15:37:52 -0300

    atualizando o nome sem acento para evitar erros

--- versao_email.py
+++ versao_email.py
@@ -82,7 +82,7 @@ inspetor = mail.GetInspector
 assinatura = mail.HTMLBody
 meu_texto = (
     f"Bom dia,<br><br>"
-    f"Segue o mapa de faturamento diário automatizado do dia: {data_de_hoje}. "
+    f"Segue o mapa de faturamento diário automatizado do dia: {data_de_hoje}. <br><br>"
     f"Atenciosamente Murilo Moraes, analista de dados.<br><br>"
 )
 mail.HTMLBody = meu_texto + assinatura
