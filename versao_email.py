1eccf87e37a01161f5a99b448a676beb80d83100
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-08-25 15:49:45 -0300

    comando para retirar o bug de chegar em diferentes horarios para os usuarios

--- versao_email.py
+++ versao_email.py
@@ -153,6 +153,7 @@ print(arquivo_faturamento_diario)
 print("esperando 10 segundos para poder abrir o display")
 time.sleep(10) #trocar para 30 segundos quando entrar em produção 
 mail.Send()    #decidi colocar display pra poder dar o aval e conferir o email, mas futuramente vou mandar automaticamente 
+outlook.GetNamespace("MAPI").SendAndReceive(False)
 print("--------")
 print(data_de_hoje)
 
