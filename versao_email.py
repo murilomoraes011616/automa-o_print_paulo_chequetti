7aa09106f973747c80f3499f5d32d44ae84d2cce
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-09-22 09:32:27 -0300

    adicionando destinátario solicitado pela gerencia

--- versao_email.py
+++ versao_email.py
@@ -139,6 +139,7 @@ lista_emails = [
     "patricia.pinheiro@greentech.log.br",
     "rodrigo.ferrarezzo@greentech.log.br",
     "paulo.chequetti@greentech.log.br",
+    "marcelo.mota@greentech.log.br",
 ]
 mail.To = ";".join(lista_emails) #lista de destinatarios do email, o join formaata cada valor entre ;, pois e o formato que o COM do outlook aceita 
 destinatarios = mail.To 
